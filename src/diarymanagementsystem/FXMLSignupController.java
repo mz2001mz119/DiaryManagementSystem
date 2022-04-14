3ca32630deb7c4f0c9171c7b3049bb88b6b612d0
AhmadOthman1 <103585755+AhmadOthman1@users.noreply.github.com>
2022-04-14 22:24:18 +0300

    diary

--- src/diarymanagementsystem/FXMLSignupController.java
+++ src/diarymanagementsystem/FXMLSignupController.java
@@ -56,6 +56,9 @@ public class FXMLSignupController implements Initializable {
     @FXML
     private Label EmailErrorLabel;
 
+    @FXML
+    private Label userNameErrorLabel;
+
     /**
      * Initializes the controller class.
      */
