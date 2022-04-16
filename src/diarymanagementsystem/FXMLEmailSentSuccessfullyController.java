3fd29ae0f8ec25d6897d94c87a0db9819003087a
Moheedeab <moheedeab16@gmail.com>
2022-04-16 03:53:12 +0300

    new new

--- src/diarymanagementsystem/FXMLEmailSentSuccessfullyController.java
+++ src/diarymanagementsystem/FXMLEmailSentSuccessfullyController.java
@@ -8,6 +8,8 @@ package diarymanagementsystem;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,18 +17,44 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+ 
 
 /**
  * FXML Controller class
  *
  * @author HI-TECH
  */
-public class FXMLEmailSentSuccessfullyController implements Initializable {
+public class FXMLEmailSentSuccessfullyController  implements Initializable {
 
+        public static boolean isValidPassword(String password)
+    {     String regex = "^(?=.*[0-9])"
+                       + "(?=.*[a-z])(?=.*[A-Z])"
+                       + "(?=.*[@#$%^&+=])"
+                       + "(?=\\S+$).{8,20}$";
+  
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+  
+        // If the password is empty
+        // return false
+        if (password == null) {
+            return false;
+        }
+  
+        // Pattern class contains matcher() method
+        // to find matching between given password
+        // and regular expression.
+        Matcher m = p.matcher(password);
+  
+        // Return if the password
+        // matched the ReGex
+        return m.matches();
+    }
     @FXML
     private Button backButton;
 
@@ -47,7 +75,7 @@ public class FXMLEmailSentSuccessfullyController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+   
     }    
 
     @FXML
@@ -57,18 +85,50 @@ public class FXMLEmailSentSuccessfullyController implements Initializable {
         Stage Stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         Stage.setScene(Scene);
         Stage.show();
+        
     }
     @FXML
     private void checkCode(ActionEvent event) throws IOException {
         //check the code 
-        //..
-        //..
-        //..
-        //if the code correct then:
-        Parent Parent = FXMLLoader.load(getClass().getResource("FXMLLogin.fxml"));
+        String code =confirmationCodeTextField.getText();
+        String newPassword =confirmationPasswordTextField.getText();
+        
+  
+      if(code.equals(FXMLForgotPasswordController.Random)){
+           //if the code correct then cheak password:
+        if(isValidPassword(newPassword)){
+        //set new pasword to data base if it valid 
+        
+        
+        
+       Parent Parent = FXMLLoader.load(getClass().getResource("FXMLLogin.fxml"));
         Scene Scene = new Scene(Parent);
         Stage Stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         Stage.setScene(Scene);
-        Stage.show();
+        Stage.show();}
+        
+         else{
+      Alert alert = new Alert(Alert.AlertType.ERROR);
+      alert.setTitle("Invalid password");
+      alert.setHeaderText("Invalid password ");
+      alert.setContentText(
+"  It contains at least 8 characters and at most 20 characters.\n" +
+"  It contains at least one digit.\n" +
+"  It contains at least one upper case alphabet.\n" +
+"  It contains at least one lower case alphabet.\n" +
+"  It contains at least one special character !@#$%&.\n" +
+"  It doesn’t contain any white space");
+      alert.showAndWait();}
+   
     }
+      
+      else{
+      Alert alert = new Alert(Alert.AlertType.ERROR);
+      alert.setTitle("Invalid code");
+      alert.setHeaderText("Invalid code ");
+      alert.setContentText("please enter valid code");
+      alert.showAndWait();}
+    
+    }
+    
 }
---
+++ src/diarymanagementsystem/send_email.java
@@ -0,0 +1,64 @@
+
+package diarymanagementsystem;
+
+import java.util.*;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import javax.mail.Authenticator;
+
+
+
+
+public class send_email {
+    
+   public static void sendemail(String recepient,String Random) throws MessagingException{
+
+    final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+ // Get a Properties object
+    Properties props = System.getProperties();
+    props.setProperty("mail.smtp.host", "smtp.gmail.com");
+    props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+    props.setProperty("mail.smtp.socketFactory.fallback", "false");
+    props.setProperty("mail.smtp.port", "888");
+    props.setProperty("mail.smtp.socketFactory.port", "465");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.debug", "true");
+    props.put("mail.store.protocol", "pop3");
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+    props.put("mail.smtp.starttls.enable","true"); 
+
+    final String username ="systemdiarymanagement@gmail.com" ;
+    final String password ="Sys2022Dia";
+   
+      Session session = Session.getDefaultInstance(props, 
+                          new Authenticator(){
+                             @Override
+                             protected PasswordAuthentication getPasswordAuthentication() {
+                                return new PasswordAuthentication(username, password);
+                             }});
+
+   // -- Create a new message --
+      Message msg = new MimeMessage(session);
+
+   // -- Set the FROM and TO fields --
+      msg.setFrom(new InternetAddress(username));
+      msg.setRecipients(Message.RecipientType.TO, 
+                        InternetAddress.parse( recepient ,false));
+      msg.setSubject("Wellcome to Diary Management System ");
+      msg.setText("to continue with your email verfication ,\n please enter the following code :\n"
+              + "Verfication Code:\n"+Random
+             );
+      msg.setSentDate(new Date());
+      Transport.send(msg);         
+   }
+       
+
+    
+}
